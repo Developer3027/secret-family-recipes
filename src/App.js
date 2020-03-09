e18ec3132ae9497d98536511a8cd6c791625047d
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-03-09 00:40:27 -0400

    log in, sign in, google auth all working

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
               ...snapShot.data()
             }
           }) // ,() => {console.log(this.state)} - put this in between the curly and perenth
-          console.log(this.state);
+
         });
       }else{
         this.setState({ currentUser: userAuth });
--- src/components/Login/LoginForm.component.jsx
+++ src/components/Login/LoginForm.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { signInWithGoogle } from './../../firebase/firebase.utilis';
+import { auth, signInWithGoogle } from './../../firebase/firebase.utilis';
 
 import './loginForm.styles.scss';
 import FormInput from '../inputs/form-input.components';
@@ -15,9 +15,16 @@ class LoginForm extends React.Component {
         }
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.setState({ email: '', password: '' })
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword( email, password );
+            this.setState({ email: '', password: '' })
+        }catch (error){
+            console.error('error on login: ', error.message);
+        }
     }
 
     handleChange = (e) => {
